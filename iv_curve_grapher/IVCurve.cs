a5510fe4b02ea009f7768313bc85a7b7a06d5548
kelldog <kellen.gillis@gmail.com>
2013-04-04 09:31:00 -0700

    first commit

---
+++ iv_curve_grapher/IVCurve.cs
@@ -0,0 +1,491 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using System.IO.Pipes;
+using System.Timers;
+using System.Globalization;
+using System.Xml.Serialization;
+
+
+namespace dataloopers_reader
+{
+    public class Raw_IV_Curve
+    {
+        [XmlIgnoreAttribute]
+        public const int MAX_POINTS_PER_RF_PACKET = 20;
+
+        [XmlIgnoreAttribute]
+        public List<short> v, c;
+
+        public string stat_calc_error = "none";
+
+        public DateTime SweepTime;
+
+
+        public int last_frame_added = -1;
+
+        public byte info_byte;
+
+        public ushort total_points;
+
+        public float LQI;
+        // public byte sweep_info;
+        public ushort sweep_time;
+
+        public ushort v_gs_end;
+
+        public byte sweep_time_scaler_per_point;
+
+        public float board_temp, vbat;
+
+        public short accel_x, accel_y, accel_z;
+
+        public int skipped_frame_at = -1;
+
+        public ushort id;
+
+        public float irradiation_mv;
+
+        public bool is_irradation_valid;
+
+        public float irradiation_mv_stdev_lastmin;
+
+        public float Vmpp, Impp;
+
+        public bool isCharging;
+
+
+        [XmlIgnoreAttribute]
+        public int total_points_added_so_far;
+
+        [XmlIgnoreAttribute]
+        public int sum_LQI = 0;
+
+
+        public volatile bool complete_curve;
+
+
+        public volatile bool written_to_db;
+
+        [XmlIgnoreAttribute]
+        public long first_packet_ms;
+
+        [XmlIgnoreAttribute]
+        public long last_packet_ms;
+
+
+        public byte hardware_version;
+        public long digi_seconds;
+
+        public ushort address;
+
+        public ushort v_thresh = 0;
+
+        public UInt32 power_dissipation = 0;
+
+        public string ModuleName = "none";
+
+        public float ActiveArea = 1.0f;
+
+        public DateTime ModuleInstallDate;
+
+        [XmlIgnoreAttribute]
+        public string module_description;
+
+        [XmlIgnoreAttribute]
+        public int module_id;
+
+        [XmlIgnoreAttribute]
+        public System.Timers.Timer curve_incomplete_timer;
+
+        public float sense_r;
+
+        public float dV;
+
+        public float dI;
+
+        public float calibration;
+
+        public float accuracy_scaler = hardware_definition.accuracy_scaler;
+
+        public float Voc, Isc, PeakPower, Rsc, Rsh, FF;
+
+
+        public float[] currents;
+        public float[] voltages;
+
+        public int frames_received = 0;
+
+        public string sweep_direction = "";
+
+
+
+        public byte[] get_curve_as_raw_bytes()
+        {
+            //using little endian
+            int loc = 0;
+            byte[] curve = new byte[4 * v.Count];
+            for (int i = 0; i < v.Count; i++)
+            {
+                curve[loc++] = (byte)v[i];
+                curve[loc++] = (byte)(v[i] >> 8);
+            }
+            for (int i = 0; i < c.Count; i++)
+            {
+                curve[loc++] = (byte)c[i];
+                curve[loc++] = (byte)(c[i] >> 8);
+            }
+            return curve;
+        }
+
+        public void calc_curve_stats()
+        {
+            Voc = (v[0] * dV);
+            Isc = c[c.Count - 1] * dI;
+            long peakpower = 0;
+            int ip = 0;
+            long power;
+            for (int i = 0; i < c.Count; i++)
+            {
+                power = v[i] * c[i];
+                if (peakpower < power)
+                {
+                    ip = i;
+                    peakpower = power;
+
+                }
+            }
+            PeakPower = v[ip] * dV * c[ip] * dI;
+            Vmpp = v[ip] * dV;
+            Impp = c[ip] * dI;
+            try
+            {
+                FF = PeakPower / (Isc * Voc);
+            }
+            catch
+            {
+                FF = 0;
+            }
+
+            try
+            {
+                int hi = 2;
+                int lo = 0;
+                Rsc = Math.Abs((float)(v[hi] - v[lo]) / (float)(c[hi] - c[lo]));
+                Rsc = Rsc * (dV / dI);
+
+            }
+            catch
+            {
+
+            }
+
+            try
+            {
+                int limit = v.Count - 1;
+                int spread = 3;
+                Rsh = Math.Abs((float)(v[limit] - v[limit - spread]) / (float)(c[limit] - c[limit - spread]));
+                Rsh = Rsh * (dV / dI);
+            }
+            catch { }
+            if (float.IsInfinity(Rsh))
+            {
+                Rsh = -1;
+            }
+
+            LQI = (float)(sum_LQI / frames_received);
+
+        }
+        public void WriteDataToExcel(string filename)
+        {
+
+            StreamWriter wr = new System.IO.StreamWriter(filename);
+            wr.WriteLine("Current (A),Voltage (V)");
+            try
+            {
+                for (int i = 0; i < total_points; i++)
+                {
+                    wr.WriteLine(string.Format("{0},{1}", currents[i].ToString("F4", CultureInfo.InvariantCulture), voltages[i].ToString("F4", CultureInfo.InvariantCulture)));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error writing csv " + ex.Message);
+            }
+            finally
+            {
+                wr.Close();
+            }
+            Console.WriteLine("wrote csv file " + filename);
+        }
+
+        [XmlIgnoreAttribute]
+        public float average_LQI
+        {
+            get
+            {
+                float ave = 0;
+                return ave;
+            }
+        }
+
+        [XmlIgnoreAttribute]
+        public long total_ms_receive_time
+        {
+            get
+            {
+                return (last_packet_ms - first_packet_ms) / 10000;
+            }
+        }
+        public Raw_IV_Curve()
+        {
+
+
+        }
+        //public Raw_IV_Curve(ushort id_in, int total_points_in, ushort address_in, byte hardware_version_in)
+        public Raw_IV_Curve(ushort address_in)
+        {
+            SweepTime = DateTime.Now;
+
+            first_packet_ms = DateTime.Now.Ticks;
+            total_points_added_so_far = 0;
+            complete_curve = false;
+            written_to_db = false;
+            address = address_in;
+            curve_incomplete_timer = new System.Timers.Timer(10000);
+            curve_incomplete_timer.Elapsed += new ElapsedEventHandler(curve_incomplete_timer_Elapsed);
+            curve_incomplete_timer.Start();
+
+
+            //frames = new List<Raw_IV_Curve_Frame>();
+
+
+        }
+        public void frame_zero(byte[] raw_data, ref int loc, int points_in_frame)
+        {
+
+
+            hardware_version = raw_data[loc];
+            loc += 1;
+
+            sweep_time_scaler_per_point = raw_data[loc];
+            loc += 1;
+
+            total_points = BitConverter.ToUInt16(raw_data, loc);
+            loc += 2;
+
+            sweep_time = raw_data[loc];
+            loc += 1;
+
+            info_byte = raw_data[loc];
+            loc += 1;
+
+
+            if (((byte)info_byte & (byte)0x01) == (byte)0x01)
+            {
+                sweep_direction = "VOC_TO_ISC";
+            }
+            else
+            {
+                sweep_direction = "ISC_TO_VOC";
+            }
+
+            if (((byte)info_byte & (byte)0x80) == (byte)0x80)
+            {
+                isCharging = true;
+            }
+            else
+            {
+                isCharging = false;
+            }
+
+            vbat = BitConverter.ToUInt16(raw_data, loc) * 3.3f / 1023f;
+            loc += 2;
+
+            board_temp = 100f * (BitConverter.ToUInt16(raw_data, loc) * 3.3f / 1023f - .5f);
+            loc += 2; //(get_voltage(raw_data, 5) - 0.5f) * 100f;
+
+            accel_x = BitConverter.ToInt16(raw_data, loc);
+            loc += 2;
+
+            accel_y = BitConverter.ToInt16(raw_data, loc);
+            loc += 2;
+
+            accel_z = BitConverter.ToInt16(raw_data, loc);
+            loc += 2;
+
+            v_thresh = BitConverter.ToUInt16(raw_data, loc);
+            loc += 2;
+
+            v_gs_end = BitConverter.ToUInt16(raw_data, loc);
+            loc += 2;
+
+            sweep_time = BitConverter.ToUInt16(raw_data, loc);
+            loc += 2;
+
+            power_dissipation = BitConverter.ToUInt32(raw_data, loc);
+            loc += 4;
+
+            v = new List<short>();
+            c = new List<short>();
+
+            for (int h = 0; h < points_in_frame; h++)
+            {
+                v.Add(BitConverter.ToInt16(raw_data, loc));
+                loc += 2;
+
+                c.Add(BitConverter.ToInt16(raw_data, loc));
+                loc += 2;
+            }
+
+            //todo: add LQI averaging here
+        }
+
+        public void other_frames(byte[] raw_data, ref int loc, int points_in_frame)
+        {
+            for (int h = 0; h < points_in_frame; h++)
+            {
+                v.Add(BitConverter.ToInt16(raw_data, loc));
+                loc += 2;
+
+                c.Add(BitConverter.ToInt16(raw_data, loc));
+                loc += 2;
+            }
+            //todo: add LQI averaging here
+        }
+
+        public bool AddFrame(byte[] raw_data)
+        {
+            int loc = 0;
+
+            id = BitConverter.ToUInt16(raw_data, loc);
+            loc += 2;
+
+
+
+            int frame_number = raw_data[loc];
+            loc += 1;
+
+            int points_in_frame = raw_data[loc];
+            loc += 1;
+
+            if (frame_number == last_frame_added)
+            {
+                Console.WriteLine("***************DUPLICATE FRAME, IGNORING************");
+                return true;
+            }
+            else if (frame_number > last_frame_added + 1)
+            {
+                skipped_frame_at = last_frame_added + 1;
+                Console.WriteLine("***************SKIPPED A FRAME IN CURVE************");
+                return false;
+            }
+
+            last_frame_added = frame_number;
+
+
+            if (frame_number == 0)
+            {
+
+                frame_zero(raw_data, ref loc, points_in_frame);
+            }
+            else
+            {
+                other_frames(raw_data, ref loc, points_in_frame);
+            }
+
+            if (raw_data.Length - 1 == loc)
+            {
+                sum_LQI += raw_data[loc];
+            }
+            digi_seconds = -1;
+            try
+            {
+                string digi_seconds_time_string = ASCIIEncoding.ASCII.GetString(raw_data, loc + 1, raw_data.Length - loc - 2);
+                digi_seconds_time_string = digi_seconds_time_string.Trim('\"');
+                digi_seconds = (long)float.Parse(digi_seconds_time_string);
+            }
+            catch (Exception ex)
+            {
+                digi_seconds = -2;
+            }
+
+
+
+
+            total_points_added_so_far += points_in_frame;
+
+            frames_received++;
+
+
+
+            if (total_points_added_so_far == total_points)
+            {
+                complete_curve = true;
+                curve_incomplete_timer.Stop();
+                last_packet_ms = DateTime.Now.Ticks;
+
+                voltages = new float[total_points];
+                currents = new float[total_points];
+
+                hardware_definition def = Program.hardware_dict[hardware_version];
+
+                dI = def.dI;
+                dV = def.dV;
+
+                sense_r = def.sense_r;
+
+
+                for (int i = 0; i < total_points; i++)
+                {
+                    currents[i] = Program.GetCurrentFromADC(c[i], def);
+                    voltages[i] = Program.GetVoltageFromADC(v[i], def);
+                }
+
+                if (Program.modules != null)
+                {
+                    try
+                    {
+                        ActiveArea = Program.modules[address].active_area;
+                        ModuleName = Program.modules[address].name;
+                        ModuleInstallDate = Program.modules[address].install_date_time;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(string.Format("error looking up module in dictionary for address {0}.\r\n {1} ", address, ex.Message));
+                    }
+
+                }
+
+
+                try
+                {
+                    calc_curve_stats();
+                }
+                catch (Exception ex)
+                {
+                    stat_calc_error = ex.Message + " : " + ex.StackTrace;
+                }
+            }
+
+            return true;
+        }
+        void curve_incomplete_timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            curve_incomplete_timer.Stop();
+            Console.WriteLine(string.Format("TIMER ELAPSED  with {0} frame received for unit with address {1}", frames_received, Program.hex(address)));
+            lock (Program.curves)
+            {
+                Program.curves.Remove(this.id);
+            }
+        }
+    }
+}
+
+
+*/
---
+++ iv_curve_grapher/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+//using dataloopers_reader;
+using System.Xml.Serialization;
+
+
+namespace iv_curve_grapher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string dir = Directory.GetCurrentDirectory() +"\\iv_data";//
+
+           // dir = @"C:\Users\cowdo\Documents\WirelessTracer\iv_catcher\iv_curve_catcher\iv_curve_catcher\bin\Debug\iv_data\";
+            
+            FileSystemWatcher watcher = new FileSystemWatcher(dir);
+
+            string pgm = @"C:\Program Files\gnuplot\bin\pgnuplot.exe";
+            if (args.Length > 0)
+            {
+                pgm = args[0];
+            }
+
+            Console.WriteLine("using gnu plot @ " + pgm);
+            Thread.Sleep(400);
+            Process extPro = new Process();
+            extPro.StartInfo.FileName = pgm;
+            extPro.StartInfo.UseShellExecute = false;
+            extPro.StartInfo.RedirectStandardInput = true;
+            extPro.Start();
+
+           // XmlSerializer iv_xml = new XmlSerializer(typeof(Raw_IV_Curve));
+
+
+            StreamWriter gnupStWr = extPro.StandardInput;
+            while (true)
+            {
+
+                WaitForChangedResult res = watcher.WaitForChanged(WatcherChangeTypes.All,int.MaxValue);
+                if (res.TimedOut)
+                {
+                    continue;
+                }
+                if (res.Name.Contains(".csv"))
+                {
+                    Thread.Sleep(1000);
+
+                    try
+                    {
+                        string fn = Path.Combine(dir, res.Name);
+
+                        string[] lines = File.ReadAllLines(fn);
+
+                        DateTime filetime = File.GetCreationTime(fn);
+
+                        List<float> V = new List<float>();
+                        List<float> C = new List<float>();
+
+                        float Vmpp = 0;
+                        float Impp = 0;
+                        float mpp = 0;
+
+                        foreach (string l in lines)
+                        {
+                            try
+                            {
+                                string[] f = l.Split(',');
+                                float v = float.Parse(f[1]);
+                                float c = float.Parse(f[0]);
+                                V.Add(v);
+                                C.Add(c);
+                                if (c * v > mpp)
+                                {
+                                    mpp = c * v;
+                                    Vmpp = v;
+                                    Impp = c;
+
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+
+                            }
+
+
+                        }
+
+                        //  TextReader reader = new StreamReader( fn );
+                        //  Raw_IV_Curve curve = (Raw_IV_Curve)iv_xml.Deserialize(reader);
+
+                        //  string csv_file = res.Name.Replace(".txt", ".csv");
+
+
+
+                        int index = res.Name.LastIndexOf('a');
+                        string address = res.Name.Substring(index + 1, res.Name.LastIndexOf('.') - index);
+                        gnupStWr.WriteLine("reset");
+                        gnupStWr.Flush();
+                        gnupStWr.WriteLine("set term wxt " + address);
+                        gnupStWr.Flush();
+
+                        gnupStWr.WriteLine("set style data linespoints");
+                        gnupStWr.Flush();
+                        gnupStWr.WriteLine("set grid");
+                        gnupStWr.Flush();
+                        gnupStWr.WriteLine("set datafile separator \",\"");
+                        gnupStWr.Flush();
+                        gnupStWr.WriteLine("set output \"last_iv.png\"");
+                        gnupStWr.Flush();
+                        gnupStWr.WriteLine(string.Format("set title \"File : {0}\"", res.Name));
+                        gnupStWr.Flush();
+
+                        gnupStWr.WriteLine("set xlabel \"Voltage(V)\"");
+                        gnupStWr.Flush();
+                        gnupStWr.WriteLine("set ylabel \"Current(A)\"");
+                        gnupStWr.Flush();
+                        gnupStWr.WriteLine(string.Format("set label \"  Peak Power = {0} Watts \" at {1},{2}", mpp.ToString("F2"), Vmpp, Impp));
+
+                        gnupStWr.Flush();
+                        gnupStWr.WriteLine("set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 ps .4");
+                        gnupStWr.Flush();
+                        gnupStWr.WriteLine("set autoscale");
+                        gnupStWr.Flush();
+                        //string file = fn;// @"C:\Users\cowdo\Documents\WirelessTracer\iv_catcher\iv_curve_catcher\iv_curve_catcher\bin\Debug\iv_data\2013_77_47895_a13.csv";
+                        gnupStWr.WriteLine(string.Format("plot '{0}' using 2:1 title \'{1}  {2}\' with linespoints ls 1", fn, filetime.ToShortDateString(), filetime.ToShortTimeString()));
+                        gnupStWr.Flush();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
